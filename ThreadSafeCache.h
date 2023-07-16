04c3d7082c20e22a71726398e6abbb7d21cc7773
Keith Jeffery <krjeffery@gmail.com>
2023-07-15 20:08:53 -0700

    "Update ThreadSafeCache class for concurrency and performance improvements"
    The ThreadSafeCache class has been updated with significant modifications to improve thread safety and performance. We have included the <shared_mutex> header file to provide us multiple-read/single-write semantics. As part of this, mutex has been added to handle specific locking/unlocking in method for better thread safety.
    
    We also have revamped the lookup() method to now use hashing which improves cache performance for large data sets. Further, the rehash() function has been modified to ensure safe and effectual rehashing without invalidating references.
    
    This revision seeks to resolve or prevent issues of data race and improve cache performance.

--- ThreadSafeCache.h
+++ ThreadSafeCache.h
@@ -3,6 +3,7 @@
 #include <cassert>
 #include <cmath>
 #include <forward_list>
+#include <shared_mutex>
 #include <vector>
 
 #define expects(x) assert(x)
@@ -12,7 +13,11 @@ template <typename Key, typename T>
 class ThreadSafeCache
 {
 public:
-    using size_type = std::size_t;
+    using size_type       = std::size_t;
+    using hasher          = std::hash<Key>; // TODO: template
+    using value_type      = std::pair<const Key, T>;
+    using reference       = value_type&;
+    using const_reference = const value_type&;
 
     explicit ThreadSafeCache(size_type bucket_count)
     : m_buckets(std::max(bucket_count, size_type(1)))
@@ -30,22 +35,54 @@ public:
     {
     }
 
+    // TODO: can these return references? We have to be careful about rehashing not invalidating references.
     template <typename F>
-    const T& lookup(const Key& key, F&& creator) const
+    const T& lookup(const Key& key, F&& creator)
     {
         // Read lock on bucket list
+        std::shared_lock<SharedMutex> bucket_lock(m_bucket_mutex);
+
+        const auto bucket_count = m_buckets.size();
+        const auto bucket_idx   = get_bucket_index(hasher{}(key), bucket_count);
+
+        LockingList& locking_list = m_buckets[bucket_idx];
+        ElementList& element_list = locking_list.m_list;
+
         // Write lock on linked list
+        std::unique_lock<SharedMutex> list_lock(locking_list.m_mutex);
 
         // Lookup value. If there, return
+
+        auto compare = [&key](const auto& r) { return r.first == key; };
+
+        const auto it = std::find_if(element_list.cbegin(), element_list.cend(), compare);
+        if (it != element_list.cend()) {
+            return it->second;
+        }
+
         // Else create
         // No sentinel needed: we have a write lock
 
-        // Read lock still active...
-        // If we are over our bucket capacity
-        // Create new
+        element_list.emplace_front(key, creator(key));
 
-        // Release read lock
-        // Swap
+        // emplace_front does not return anything until C++17, so do it the hard way...
+        const T& ret = element_list.front().second;
+
+        std::size_t num_elements;
+        {
+            std::unique_lock<SharedMutex> num_elements_lock(m_num_elements_mutex);
+            num_elements = ++m_num_elements;
+        }
+
+        if (num_elements > max_load_factor() * bucket_count) {
+            // Undo list lock
+            locking_list.m_mutex.unlock();
+            // Undo bucket lock
+            m_bucket_mutex.unlock();
+            rehash(bucket_count * 3u / 2u);
+        }
+
+        return ret;
     }
 
     const T& lookup(const Key& key) const
@@ -81,8 +118,25 @@ public:
         m_max_load_factor = ml;
     }
 
-    void rehash(size_type count)
+    void rehash(size_type new_bucket_count)
     {
+        std::unique_lock<SharedMutex> bucket_lock(m_bucket_mutex);
+        if (m_buckets.size() >= new_bucket_count) {
+            return;
+        }
+        BucketList new_locking_list(new_bucket_count);
+
+        for (auto& bucket : m_buckets) {
+            ElementList& old_list = bucket.m_list;
+            while (!old_list.empty()) {
+                const auto hash = hasher{}(old_list.cbegin()->first);
+                const auto new_bucket_idx = get_bucket_index(hash, new_bucket_count);
+                ElementList& new_list = new_locking_list[new_bucket_idx].m_list;
+                new_list.splice_after(new_list.cbefore_begin(), old_list, old_list.cbefore_begin());
+            }
+        }
+
+        m_buckets.swap(new_locking_list);
     }
 
     void reserve(size_type count)
@@ -92,8 +146,17 @@ public:
 private:
     static constexpr size_type k_default_bucket_count{32};
 
-    using ElementList = std::forward_list<T>;
-    using BucketList  = std::vector<ElementList>;
+    using SharedMutex = std::shared_timed_mutex;
+
+    using ElementList = std::forward_list<value_type>;
+
+    struct LockingList
+    {
+        ElementList         m_list;
+        mutable SharedMutex m_mutex;
+    };
+
+    using BucketList = std::vector<LockingList>;
 
     static double mod1(double x) noexcept
     {
@@ -107,6 +170,10 @@ private:
         return static_cast<std::size_t>(num_buckets * mod1(hash * phi));
     }
 
-    float m_max_load_factor{1.0f};
-    BucketList m_buckets;
+    mutable SharedMutex m_bucket_mutex;
+    mutable SharedMutex m_num_elements_mutex;
+
+    float       m_max_load_factor{1.0f};
+    std::size_t m_num_elements{0};
+    BucketList  m_buckets;
 };
