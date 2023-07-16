b7654db6c6d9b81baa843eac66615b92fd4efb6f
Keith Jeffery <krjeffery@gmail.com>
2023-07-16 15:48:56 -0700

    Renamed and improved thread-safe cache functions
    Renamed the lookup function to find_or_create to clarify its purpose. lookup function was split into two: find_or_create and at. at function was added for locating values, throwing an exception if it can't find a key instead of creating a new one. The reserve function was modified to consider max_load_factor when determining rehash size, enhancing efficiency. These changes improve code clarity and the efficiency of the cache function.

--- ThreadSafeCache.h
+++ ThreadSafeCache.h
@@ -37,7 +37,7 @@ public:
 
     // TODO: can these return references? We have to be careful about rehashing not invalidating references.
     template <typename F>
-    const T& lookup(const Key& key, F&& creator)
+    const T& find_or_create(const Key& key, F&& creator)
     {
         // Read lock on bucket list
         std::shared_lock<SharedMutex> bucket_lock(m_bucket_mutex);
@@ -79,18 +79,52 @@ public:
             locking_list.m_mutex.unlock();
             // Undo bucket lock
             m_bucket_mutex.unlock();
+
+            // TODO: max of this and load_factor
             rehash(bucket_count * 3u / 2u);
         }
 
         return ret;
     }
 
-    const T& lookup(const Key& key) const
+    // C++17: optional
+#if 0
+    const T& find(const Key& key) const
     {
         // Read lock on bucket list
         // Read lock on linked list
 
         // Lookup value. If not there, throw
+    }
+#endif
+
+    const T& at(const Key& key) const
+    {
+        // Read lock on bucket list
+        std::shared_lock<SharedMutex> bucket_lock(m_bucket_mutex);
+
+        const auto bucket_count = m_buckets.size();
+        const auto bucket_idx   = get_bucket_index(hasher{}(key), bucket_count);
+
+        LockingList& locking_list = m_buckets[bucket_idx];
+        ElementList& element_list = locking_list.m_list;
+
+        // Read lock on linked list
+        std::shared_lock<SharedMutex> list_lock(locking_list.m_mutex);
+
+        // Lookup value. If there, return
+        auto compare = [&key](const auto& r) { return r.first == key; };
+
+        const auto it = std::find_if(element_list.cbegin(), element_list.cend(), compare);
+        if (it == element_list.cend()) {
+            throw std::out_of_range{"No key for 'at'"};
+        }
+        return it->second;
+    }
+
+    void update(const Key& key, T value)
+    {
+
     }
 
     // While technically thread-safe, use this with caution if there are other active threads.
@@ -126,6 +160,8 @@ public:
         }
         BucketList new_locking_list(new_bucket_count);
 
+        // We go through the trouble of splicing the lists so that we don't invalidate references held outside of the
+        // class.
         for (auto& bucket : m_buckets) {
             ElementList& old_list = bucket.m_list;
             while (!old_list.empty()) {
@@ -139,8 +175,9 @@ public:
         m_buckets.swap(new_locking_list);
     }
 
-    void reserve(size_type count)
+    void reserve(size_type element_count)
     {
+        rehash(static_cast<size_type>(std::ceil(element_count / max_load_factor())));
     }
 
 private:
