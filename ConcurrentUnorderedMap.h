006b747d9aea52530f829234b33e357e65660c56
Keith Jeffery <krjeffery@gmail.com>
2023-08-28 17:02:09 -0700

    Remove unused hardware_constructive_interference_size
    The unused hardware_constructive_interference_size is being removed from the ConcurrentUnorderedMap.h file.

--- ConcurrentUnorderedMap.h
+++ ConcurrentUnorderedMap.h
@@ -101,11 +101,9 @@ inline const T& get_value(const std::pair<T, U>& v) noexcept
 }
 
 #ifdef __cpp_lib_hardware_interference_size
-using std::hardware_constructive_interference_size;
 using std::hardware_destructive_interference_size;
 #else
 // 64 bytes on x86-64 │ L1_CACHE_BYTES │ L1_CACHE_SHIFT │ __cacheline_aligned │ ...
-constexpr std::size_t hardware_constructive_interference_size = 64;
 constexpr std::size_t hardware_destructive_interference_size  = 64;
 #endif
 
